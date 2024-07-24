91139c5fee6e14246215d91f66a0beeb7f20798c
rickparrish <rickparrish@users.noreply.github.com>
2024-07-24 09:39:30 -0400

    Update path and messaging

--- htdocs/luci-static/resources/view/adblock-lean/config.js
+++ htdocs/luci-static/resources/view/adblock-lean/config.js
@@ -8,18 +8,18 @@ let notMsg, errMsg;
 return view.extend({
 	load: function () {
 		return Promise.all([
-			L.resolveDefault(fs.stat('/root/adblock-lean/allowlist'), {}),
-			L.resolveDefault(fs.read_direct('/root/adblock-lean/allowlist'), '')
+			L.resolveDefault(fs.stat('/root/adblock-lean/config'), {}),
+			L.resolveDefault(fs.read_direct('/root/adblock-lean/config'), '')
 		]);
 	},
 	handleSave: function (ev) {
-		let value = ((document.querySelector('textarea').value || '').trim().toLowerCase().replace(/\r\n/g, '\n')) + '\n';
-		return fs.write('/root/adblock-lean/allowlist', value)
+		let value = ((document.querySelector('textarea').value || '').trim().replace(/\r\n/g, '\n'));
+		return fs.write('/root/adblock-lean/config', value)
 			.then(function () {
 				document.querySelector('textarea').value = value;
 				document.body.scrollTop = document.documentElement.scrollTop = 0;
 				if (!notMsg) {
-					ui.addNotification(null, E('p', _('Allowlist modifications have been saved, reload adblock-lean for changes to take effect.')), 'info');
+					ui.addNotification(null, E('p', _('Config modifications have been saved, reload adblock-lean for changes to take effect.')), 'info');
 					notMsg = true;
 				}
 			}).catch(function (e) {
@@ -30,22 +30,22 @@ return view.extend({
 				}
 			});
 	},
-	render: function (allowlist) {
-		if (allowlist[0].size >= 100000) {
-			document.body.scrollTop = document.documentElement.scrollTop = 0;
-			ui.addNotification(null, E('p', _('The allowlist is too big, unable to save modifications.')), 'error');
-		}
+	render: function (arr) {
+		var config = arr[1] == null ? '' : arr[1];
+		var lines = config.split('\n');
+		var rows = lines.length < 25 ? 25 : lines.length + 1;
 		return E([
 			E('p', {},
-				_('This is the local adblock-lean allowlist that will permit certain domain names.<br /> \
-				<em><b>Please note:</b></em> add only exactly one domain name per line.')),
+				_('This is the local adblock-lean config file, /root/adblock-lean/config<br /> \
+				<em><b>Please note:</b></em> you should not modify <b>local_allowlist_path</b> or <b>local_blocklist_path</b> \
+				as the Allowlist and Blocklist tabs will only work with the default paths.')),
 			E('p', {},
 				E('textarea', {
 					'style': 'width: 100% !important; padding: 5px; font-family: monospace',
 					'spellcheck': 'false',
 					'wrap': 'off',
-					'rows': 25
-				}, [allowlist[1] != null ? allowlist[1] : ''])
+					'rows': rows
+				}, [config])
 			)
 		]);
 	},
