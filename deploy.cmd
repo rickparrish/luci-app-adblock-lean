0222d78bbd9b80f46715fc15a9d6e2e94d31f5d4
rickparrish <rickparrish@users.noreply.github.com>
2024-07-24 20:15:49 -0400

    Add a script to make deployment to the router easier

---
+++ deploy.cmd
@@ -0,0 +1,11 @@
+@ECHO OFF
+SETLOCAL
+SET CYGWINHOME=C:\CYGWIN64
+SET HOME=%USERPROFILE%
+SET CWOLDPATH=%PATH%
+SET PATH=%CYGWINHOME%\BIN;%PATH%
+
+scp -O -r root/* root@OpenWrt.lan:/
+scp -O -r htdocs/* root@OpenWrt.lan:/www/
+ssh root@OpenWrt.lan chmod 755 /www/luci-static/resources/view/adblock-lean
+ssh root@OpenWrt.lan chmod 644 /www/luci-static/resources/view/adblock-lean/*
