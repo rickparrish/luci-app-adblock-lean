968130002d936dfdd6ccaee1f9863b749e2d9f11
rickparrish <rickparrish@users.noreply.github.com>
2024-08-14 17:40:24 -0400

    New paths for deploy

--- deploy.cmd
+++ deploy.cmd
@@ -5,8 +5,8 @@ SET HOME=%USERPROFILE%
 SET CWOLDPATH=%PATH%
 SET PATH=%CYGWINHOME%\BIN;%PATH%
 
-scp -O -r root/* root@OpenWrt.lan:/
-scp -O -r htdocs/* root@OpenWrt.lan:/www/
+scp -O -r applications/luci-app-adblock-lean/root/* root@OpenWrt.lan:/
+scp -O -r applications/luci-app-adblock-lean/htdocs/* root@OpenWrt.lan:/www/
 scp -O -r ../adblock-lean/adblock-lean root@OpenWrt.lan:/etc/init.d/adblock-lean
 
 ssh root@OpenWrt.lan chmod 755 /usr/libexec/rpcd/luci.adblock-lean
