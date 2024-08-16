c0395ed5218f9ddbd56cee0fe24771b2546cac6c
rickparrish <rickparrish@users.noreply.github.com>
2024-08-16 17:56:54 -0400

    Add a comment about what the build.sh file is for

--- build.sh
+++ build.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# build.sh can be used to test the GitHub Actions workflow(s) locally before pushing to the repo
+
+# The action downloads luci.mk, which works fine when the action is executed via GitHub, but for
+# some reason the files aren't found when executing the action locally so this script will manually 
+# download it and then clean up after the build completes.
+
 # Download luci.mk
 wget https://raw.githubusercontent.com/openwrt/luci/openwrt-23.05/luci.mk
 
-# Execute 'act' to run the github action locally
+# Execute 'act' to run the GitHub action locally
+# Requires https://github.com/nektos/act
 ~/bin/act
 
 # Delete luci.mk
