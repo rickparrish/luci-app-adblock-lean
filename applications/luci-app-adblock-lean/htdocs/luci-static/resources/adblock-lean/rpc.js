9395197d98740aa834afda56ed52c4d6bb445d91
rickparrish <rickparrish@users.noreply.github.com>
2025-02-01 15:55:27 -0500

    Move status rpc functions into rpc class Move global functions into class

--- applications/luci-app-adblock-lean/htdocs/luci-static/resources/adblock-lean/rpc.js
+++ applications/luci-app-adblock-lean/htdocs/luci-static/resources/adblock-lean/rpc.js
@@ -2,6 +2,12 @@
 'require rpc';
 
 return L.Class.extend({
+	getStatus: rpc.declare({
+		object: 'luci.adblock-lean',
+		method: 'getStatus',
+		params: [],
+	}),
+
 	install: rpc.declare({
 		object: 'luci.adblock-lean',
 		method: 'install',
@@ -11,5 +17,17 @@ return L.Class.extend({
 	systemInfo: rpc.declare({
 		object: 'system',
 		method: 'info'
+	}),
+
+	updateAdblockLean: rpc.declare({
+		object: 'luci.adblock-lean',
+		method: 'updateAdBlockLean',
+		params: [],
+	}),
+	
+	updateLuciApp: rpc.declare({
+		object: 'luci.adblock-lean',
+		method: 'updateLuciApp',
+		params: ['url'],
 	}),
 });
--- applications/luci-app-adblock-lean/htdocs/luci-static/resources/adblock-lean/status.js
+++ applications/luci-app-adblock-lean/htdocs/luci-static/resources/adblock-lean/status.js
@@ -1,122 +1,102 @@
 'use strict';
 'require baseclass';
 'require fs';
-'require rpc';
 'require ui';
+'require adblock-lean.rpc as rpc';
 
 const supportedConfigFormat = 6;
 
-var getStatus = rpc.declare({
-	object: 'luci.adblock-lean',
-	method: 'getStatus',
-	params: [],
-});
-
-var update_abl = rpc.declare({
-	object: 'luci.adblock-lean',
-	method: 'updateAdBlockLean',
-	params: [],
-});
-
-var update_laabl = rpc.declare({
-	object: 'luci.adblock-lean',
-	method: 'updateLuciApp',
-	params: ['url'],
-});
-
-var statusResult = null;
-var laablLatestResult = null;
-
-async function handleAction(actionName, actionLabel) {
-	ui.showModal(null, [
-		E('p',
-			{ class: 'spinning' },
-			_(actionLabel + ' AdBlock Lean...')
-		),
-	]);
-	await fs.exec_direct('/etc/init.d/adblock-lean', [actionName]);
-	location.reload();
-}
-
-async function handleRpc(actionFunc, actionLabel) {
-	ui.showModal(null, [
-		E('p',
-			{ class: 'spinning' },
-			actionLabel
-		),
-	]);
-	L.resolveDefault(actionFunc())
-		.then(function (result) { location.reload() });
-}
-
-function setLaablUpdateStatus(showButtons) {
-	if (statusResult && laablLatestResult) {
-		var updateStatus = document.getElementById('laabl-update-status');
-
-		/*
-		If the luci app is not installed, then statusResult.laabl_package_info will be blank.
-		This shouldn't ever happen to anyone but me, so we'll report it as an error condition.
-		*/
-		if (!statusResult.laabl_package_info) {
-			updateStatus.textContent = _('An error occurred while checking update status (missing package info)');
-			return;
-		}
-
-		/*
-		statusResult.laabl_package_info will look like this:
-			Package: luci-app-adblock-lean
-			Version: git-24.229.78998-f9aed0d
-			Depends: libc, luci-base
-			Status: install user installed
-			Architecture: all
-			Installed-Time: 1723846085
-		So we need to parse the Version: line
-		*/
-		var currentVersion = statusResult.laabl_package_info.match(/Version[:]\s?(.*?)\s/)[1];
-		if (!currentVersion) {
-			updateStatus.textContent = _('An error occurred while checking update status (missing current version)');
-			return;
-		}
-
-		/*
-		laablLatestResult will contain this (snipped irrelevant bits):
-			{
-				"assets": [
-					{
-						"name": "luci-app-adblock-lean_git-24.229.78998-f9aed0d_all.ipk",
-						"browser_download_url": "https://github.com/rickparrish/luci-app-adblock-lean/releases/download/latest/luci-app-adblock-lean_git-24.229.78998-f9aed0d_all.ipk"
-					}
-				],
-			}
-		So we need to check assets[0].name to see if it contains currentVersion.  If it does, we're up to date.  If it doesn't, we can update using assets[0].browser_download_url
-		*/
-		if (laablLatestResult.assets[0].name.indexOf(currentVersion) == -1) {
-			updateStatus.textContent = _('An update is available');
-
-			if (showButtons) {
-				document.getElementById('update-laabl-button').style.display = 'inline-block';
-			}
-		} else {
-			updateStatus.textContent = _('Up to date');
-		}
-	}
-}
-
-async function update_laabl_with_url() {
-	await update_laabl(laablLatestResult.assets[0].browser_download_url);
-}
-
 var statusClass = baseclass.extend({
 	showButtons: false,
-
 	showTitle: false,
 
+	statusResult: null,
+	latestLuciAppResult: null,
+	
+	handleAction: async function(actionName, actionLabel) {
+		ui.showModal(null, [
+			E('p',
+				{ class: 'spinning' },
+				_(actionLabel + ' AdBlock Lean...')
+			),
+		]);
+		await fs.exec_direct('/etc/init.d/adblock-lean', [actionName]);
+		location.reload();
+	},
+	
+	handleRpc: async function(actionFunc, actionLabel) {
+		ui.showModal(null, [
+			E('p',
+				{ class: 'spinning' },
+				actionLabel
+			),
+		]);
+		L.resolveDefault(actionFunc()).then(function (result) { location.reload() });
+	},
+	
+	setLuciAppUpdateStatus: function(showButtons) {
+		if (this.statusResult && this.latestLuciAppResult) {
+			var updateStatus = document.getElementById('laabl-update-status');
+	
+			/*
+			If the luci app is not installed, then this.statusResult.laabl_package_info will be blank.
+			This shouldn't ever happen to anyone but me, so we'll report it as an error condition.
+			*/
+			if (!this.statusResult.laabl_package_info) {
+				updateStatus.textContent = _('An error occurred while checking update status (missing package info)');
+				return;
+			}
+	
+			/*
+			this.statusResult.laabl_package_info will look like this:
+				Package: luci-app-adblock-lean
+				Version: git-24.229.78998-f9aed0d
+				Depends: libc, luci-base
+				Status: install user installed
+				Architecture: all
+				Installed-Time: 1723846085
+			So we need to parse the Version: line
+			*/
+			var currentVersion = this.statusResult.laabl_package_info.match(/Version[:]\s?(.*?)\s/)[1];
+			if (!currentVersion) {
+				updateStatus.textContent = _('An error occurred while checking update status (missing current version)');
+				return;
+			}
+	
+			/*
+			this.latestLuciAppResult will contain this (snipped irrelevant bits):
+				{
+					"assets": [
+						{
+							"name": "luci-app-adblock-lean_git-24.229.78998-f9aed0d_all.ipk",
+							"browser_download_url": "https://github.com/rickparrish/luci-app-adblock-lean/releases/download/latest/luci-app-adblock-lean_git-24.229.78998-f9aed0d_all.ipk"
+						}
+					],
+				}
+			So we need to check assets[0].name to see if it contains currentVersion.  If it does, we're up to date.  If it doesn't, we can update using assets[0].browser_download_url
+			*/
+			if (this.latestLuciAppResult.assets[0].name.indexOf(currentVersion) == -1) {
+				updateStatus.textContent = _('An update is available');
+	
+				if (showButtons) {
+					document.getElementById('update-laabl-button').style.display = 'inline-block';
+				}
+			} else {
+				updateStatus.textContent = _('Up to date');
+			}
+		}
+	},
+	
+	updateAdblockLean: async function () {
+		await rpc.updateAdblockLean(this.latestLuciAppResult.assets[0].browser_download_url);
+	},
+	
 	render: function () {
 		// Build the title element, if showTitle is true
 		var titleElement = this.showTitle ? E('h2', {}, _('AdBlock Lean - Status')) : E('div');
 
 		// Build the table element with the status placeholders
-		var tableElement = E('table', { 'class': 'table' }, [
+		var tableElement = E('table', { 'class': 'table', 'style': 'margin-bottom: 5px;' }, [
 			E('tr', { 'class': 'tr' }, [
 				E('td', { 'class': 'td left', 'width': '33%' }, _('Service Status')),
 				E('td', { 'class': 'td left spinning', 'id': 'service-status' }, '\xa0')
@@ -145,13 +125,13 @@ var statusClass = baseclass.extend({
 			buttonElements = E('div', { class: 'right' }, [
 				E('button', {
 					'class': 'btn cbi-button cbi-button-positive',
-					'click': ui.createHandlerFn(this, function () { return handleAction('enable', 'Enabling'); }),
+					'click': ui.createHandlerFn(this, function () { return this.handleAction('enable', 'Enabling'); }),
 					'style': 'display: none',
 					'id': 'enable-button',
 				}, [_('Enable Service')]),
 				E('button', {
 					'class': 'btn cbi-button cbi-button-negative',
-					'click': ui.createHandlerFn(this, function () { return handleAction('disable', 'Disabling'); }),
+					'click': ui.createHandlerFn(this, function () { return this.handleAction('disable', 'Disabling'); }),
 					'style': 'display: none',
 					'id': 'disable-button',
 				}, [_('Disable Service')]),
@@ -161,31 +141,31 @@ var statusClass = baseclass.extend({
 				'\xa0',
 				E('button', {
 					'class': 'btn cbi-button cbi-button-positive',
-					'click': ui.createHandlerFn(this, function () { return handleAction('start', 'Activating'); }),
+					'click': ui.createHandlerFn(this, function () { return this.handleAction('start', 'Activating'); }),
 					'style': 'display: none',
 					'id': 'start-button',
 				}, [_('Activate Blocklist')]),
 				E('button', {
 					'class': 'btn cbi-button cbi-button-positive',
-					'click': ui.createHandlerFn(this, function () { return handleAction('resume', 'Resuming'); }),
+					'click': ui.createHandlerFn(this, function () { return this.handleAction('resume', 'Resuming'); }),
 					'style': 'display: none',
 					'id': 'resume-button',
 				}, [_('Resume Blocklist')]),
 				E('button', {
 					'class': 'btn cbi-button cbi-button-action',
-					'click': ui.createHandlerFn(this, function () { return handleAction('pause', 'Pausing'); }),
+					'click': ui.createHandlerFn(this, function () { return this.handleAction('pause', 'Pausing'); }),
 					'style': 'display: none',
 					'id': 'pause-button',
 				}, [_('Pause Blocklist')]),
 				E('button', {
 					'class': 'btn cbi-button cbi-button-negative',
-					'click': ui.createHandlerFn(this, function () { return handleAction('stop', 'Deactivating'); }),
+					'click': ui.createHandlerFn(this, function () { return this.handleAction('stop', 'Deactivating'); }),
 					'style': 'display: none',
 					'id': 'stop-button',
 				}, [_('Deactivate Blocklist')]),
 				E('button', {
 					'class': 'btn cbi-button cbi-button-reload',
-					'click': ui.createHandlerFn(this, function () { return handleAction('reload', 'Reloading'); }),
+					'click': ui.createHandlerFn(this, function () { return this.handleAction('reload', 'Reloading'); }),
 					'style': 'display: none',
 					'id': 'reload-button',
 				}, [_('Reload Blocklist')]),
@@ -195,13 +175,13 @@ var statusClass = baseclass.extend({
 				'\xa0',
 				E('button', {
 					'class': 'btn cbi-button cbi-button-action',
-					'click': ui.createHandlerFn(this, function () { return handleRpc(update_abl, 'Updating AdBlock Lean...'); }),
+					'click': ui.createHandlerFn(this, function () { return this.handleRpc(rpc.updateAdblockLean, 'Updating AdBlock Lean...'); }),
 					'style': 'display: none',
 					'id': 'update-abl-button',
 				}, [_('Update AdBlock Lean')]),
 				E('button', {
 					'class': 'btn cbi-button cbi-button-action',
-					'click': ui.createHandlerFn(this, function () { return handleRpc(update_laabl_with_url, 'Updating LuCI App...'); }),
+					'click': ui.createHandlerFn(this, function () { return this.handleRpc(this.updateLuciApp, 'Updating LuCI App...'); }),
 					'style': 'display: none',
 					'id': 'update-laabl-button',
 				}, [_('Update LuCI App')]),
@@ -217,7 +197,7 @@ var statusClass = baseclass.extend({
 
 		// Call the getStatus() method via RPC, then update the status placeholders with the result
 		var that = this;
-		L.resolveDefault(getStatus())
+		L.resolveDefault(rpc.getStatus())
 			.then(function (result) {
 				var serviceStatus = document.getElementById('service-status');
 				serviceStatus.classList.remove('spinning');
@@ -323,16 +303,16 @@ var statusClass = baseclass.extend({
 					}
 				}
 
-				statusResult = result;
-				setLaablUpdateStatus(that.showButtons);
+				that.statusResult = result;
+				that.setLuciAppUpdateStatus(that.showButtons);
 			}
 		);
 
 		// Get the latest luci-app-adblock-lean release details
 		L.get('https://api.github.com/repos/rickparrish/luci-app-adblock-lean/releases/tags/latest', '', function(xhr, data) {
 			if (data) {
-				laablLatestResult = data;
-				setLaablUpdateStatus(that.showButtons);
+				that.latestLuciAppResult = data;
+				that.setLuciAppUpdateStatus(that.showButtons);
 			} else {
 				var updateStatus = document.getElementById('laabl-update-status');
 				updateStatus.textContent = _('An error occurred while checking update status (checking luci-app-adblock-lean status)');
