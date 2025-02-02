c4865b8e7cd9e8cc3e68cbd1c4ab216813a13133
rickparrish <rickparrish@users.noreply.github.com>
2025-02-02 16:52:57 -0500

    Handle case where /etc/adblock-lean/config is missing

--- applications/luci-app-adblock-lean/htdocs/luci-static/resources/adblock-lean/config.js
+++ applications/luci-app-adblock-lean/htdocs/luci-static/resources/adblock-lean/config.js
@@ -14,61 +14,68 @@ return L.Class.extend({
 	supportedConfigFormat: 6,
 
 	load: async function () {
-		this.rawConfig = await fs.read_direct('/etc/adblock-lean/config');
-		if (this.rawConfig) {
-			this.loaded = true;
+		try {
+			this.rawConfig = await fs.read_direct('/etc/adblock-lean/config');
+			if (this.rawConfig) {
+				this.loaded = true;
 
-			// Parse the config file format, converting the key=value lines into an object
-			// From: https://stackoverflow.com/a/52043870
-			var result = this.rawConfig
-				// split the data by line
-				.split("\n")
-				// filter comments
-				.filter(row => (row.trim() != '') && !row.trim().startsWith('#') && (row.indexOf('=') > 0))
-				// split each row into key and property
-				.map(row => {
-					var equalsPos = row.indexOf('=');
-					var key = row.substring(0, equalsPos);
-					var value = row.substring(equalsPos + 1);
-					return [key.trim(), helpers.getCleanValue(value)];
-				})
-				// use reduce to assign key-value pairs to a new object
-				// using Array.prototype.reduce
-				.reduce((acc, [key, value]) => (acc[key] = value, acc), {});
-	
-			// *_urls need to be an array, not a space-separated string
-			result.blocklist_urls = result.blocklist_urls ? result.blocklist_urls.split(' ') : [];
-			result.blocklist_ipv4_urls = result.blocklist_ipv4_urls ? result.blocklist_ipv4_urls.split(' ') : [];
-			result.allowlist_urls = result.allowlist_urls ? result.allowlist_urls.split(' ') : [];
-			result.dnsmasq_blocklist_urls = result.dnsmasq_blocklist_urls ? result.dnsmasq_blocklist_urls.split(' ') : [];
-			result.dnsmasq_blocklist_ipv4_urls = result.dnsmasq_blocklist_ipv4_urls ? result.dnsmasq_blocklist_ipv4_urls.split(' ') : [];
-			result.dnsmasq_allowlist_urls = result.dnsmasq_allowlist_urls ? result.dnsmasq_allowlist_urls.split(' ') : [];
-
-			// We have a friendly Hagezi Blocklists multi-select, so we need to split those in blocklist_urls into hagezi_blocklists
-			result.hagezi_blocklists = [];
-			var nonHageziBlocklists = [];
-			for (var i = 0; i < result.blocklist_urls.length; i++) {
-				if (result.blocklist_urls[i].startsWith(hagezi.baseUrl)) {
-					result.hagezi_blocklists.push(result.blocklist_urls[i]);
-				} else {
-					nonHageziBlocklists.push(result.blocklist_urls[i]);
-				}
-			}
-			result.blocklist_urls = nonHageziBlocklists;
+				// Parse the config file format, converting the key=value lines into an object
+				// From: https://stackoverflow.com/a/52043870
+				var result = this.rawConfig
+					// split the data by line
+					.split("\n")
+					// filter comments
+					.filter(row => (row.trim() != '') && !row.trim().startsWith('#') && (row.indexOf('=') > 0))
+					// split each row into key and property
+					.map(row => {
+						var equalsPos = row.indexOf('=');
+						var key = row.substring(0, equalsPos);
+						var value = row.substring(equalsPos + 1);
+						return [key.trim(), helpers.getCleanValue(value)];
+					})
+					// use reduce to assign key-value pairs to a new object
+					// using Array.prototype.reduce
+					.reduce((acc, [key, value]) => (acc[key] = value, acc), {});
 		
-			// custom_script needs to be mapped to enable_custom_script
-			if (result.custom_script) {
-				result.enable_custom_script = 1;
+				// *_urls need to be an array, not a space-separated string
+				result.blocklist_urls = result.blocklist_urls ? result.blocklist_urls.split(' ') : [];
+				result.blocklist_ipv4_urls = result.blocklist_ipv4_urls ? result.blocklist_ipv4_urls.split(' ') : [];
+				result.allowlist_urls = result.allowlist_urls ? result.allowlist_urls.split(' ') : [];
+				result.dnsmasq_blocklist_urls = result.dnsmasq_blocklist_urls ? result.dnsmasq_blocklist_urls.split(' ') : [];
+				result.dnsmasq_blocklist_ipv4_urls = result.dnsmasq_blocklist_ipv4_urls ? result.dnsmasq_blocklist_ipv4_urls.split(' ') : [];
+				result.dnsmasq_allowlist_urls = result.dnsmasq_allowlist_urls ? result.dnsmasq_allowlist_urls.split(' ') : [];
+
+				// We have a friendly Hagezi Blocklists multi-select, so we need to split those in blocklist_urls into hagezi_blocklists
+				result.hagezi_blocklists = [];
+				var nonHageziBlocklists = [];
+				for (var i = 0; i < result.blocklist_urls.length; i++) {
+					if (result.blocklist_urls[i].startsWith(hagezi.baseUrl)) {
+						result.hagezi_blocklists.push(result.blocklist_urls[i]);
+					} else {
+						nonHageziBlocklists.push(result.blocklist_urls[i]);
+					}
+				}
+				result.blocklist_urls = nonHageziBlocklists;
+			
+				// custom_script needs to be mapped to enable_custom_script
+				if (result.custom_script) {
+					result.enable_custom_script = 1;
+				}
+
+				// Set the data variable in the format needed by form.JSONMap()
+				this.data.config = result;
+
+				// Set the hasSupportedConfigFormat flag
+				this.hasSupportedConfigFormat = (this.rawConfig.indexOf('config_format=v' + this.supportedConfigFormat) >= 0)
+
+				// Call the checkConfig RPC method to see if an update/reset is needed
+				this.checkConfigResult = await rpc.checkConfig();
 			}
-
-			// Set the data variable in the format needed by form.JSONMap()
-			this.data.config = result;
-
-			// Set the hasSupportedConfigFormat flag
-			this.hasSupportedConfigFormat = (this.rawConfig.indexOf('config_format=v' + this.supportedConfigFormat) >= 0)
-
-			// Call the checkConfig RPC method to see if an update/reset is needed
-			this.checkConfigResult = await rpc.checkConfig();
+		} catch (e) {
+			// Failed to read the file.  If it's NOT a NotFoundError, throw to report the error
+			if (e.name !== 'NotFoundError') {
+				throw e;
+			}
 		}
 	},
 
